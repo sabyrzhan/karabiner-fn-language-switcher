2ecd7c84c80cfffa6d7e7afa02a60c48c87205b6
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-12-19 23:03:01 +0200

    Initial commit. Added final project and Karabiner rule.

--- LanguageSwitcher/main.swift
+++ LanguageSwitcher/main.swift
@@ -6,6 +6,43 @@
 //
 
 import Foundation
+import Carbon
 
-print("Hello, World!")
+struct MyInputSource {
+    let id: String
+    let source: TISInputSource
+}
 
+func resolveInputSource(inputSource: TISInputSource) -> MyInputSource {
+    let property = TISGetInputSourceProperty(inputSource, kTISPropertyInputSourceID)!
+    let s = Unmanaged<AnyObject>.fromOpaque(property).takeUnretainedValue() as! String
+    let result = MyInputSource(id: s, source: inputSource)
+    return result
+}
+
+func getCurrentInputSource() -> MyInputSource {
+    let current = TISCopyCurrentKeyboardInputSource().takeRetainedValue()
+    let currentMyInputSource = resolveInputSource(inputSource: current)
+    return currentMyInputSource
+}
+
+func getSupportedInputSources() -> [MyInputSource] {
+    let keyboards = TISCreateInputSourceList(nil, true).takeRetainedValue() as NSArray as! [TISInputSource]
+    return keyboards.map { source -> MyInputSource in
+        let property = TISGetInputSourceProperty(source, kTISPropertyInputSourceID)!
+        let s = Unmanaged<AnyObject>.fromOpaque(property).takeUnretainedValue() as! String
+        let result = MyInputSource(id: s, source: source)
+        return result
+    }.filter { value -> Bool in
+        return value.id == "com.apple.keylayout.ABC" || value.id == "com.apple.keylayout.Kazakh"
+    };
+}
+
+let currentInputSource = getCurrentInputSource()
+let supportedInputSources = getSupportedInputSources()
+
+supportedInputSources.forEach { value in
+    if value.id != currentInputSource.id {
+        TISSelectInputSource(value.source)
+    }
+}
